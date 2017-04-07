03022b690cb71ee7cdd982eb44e94d4cef094d9a
bayo <bayo.fr@gmail.com>
2017-04-07 19:34:00 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gamepad2midi',
+    version='0.2',
+    description='Translate gamepad input to MIDI events',
+    author='Bayo',
+    author_email='bayo.fr@gmail.com',
+    url='https://github.com/bayo/gamepad2midi',
+    packages=['gamepad2midi'],
+)
