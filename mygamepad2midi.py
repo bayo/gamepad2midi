fe5e3ccb183ab043fdbc35bbf2b7f1241fe1b0af
bayo <bayo.fr@gmail.com>
2017-04-07 18:29:58 +0200

    Typo

--- mygamepad2midi.py
+++ mygamepad2midi.py
@@ -7,17 +7,18 @@ def main():
     mapping = InputMapping()
 
     # Put your gamepad name here and bind your custom mappings
-    mapping.bind_all_buttons("Logitech Logitech(R) Precision(TM) Gamepad", 9)
-    mapping.bind_axis_range("Logitech Logitech(R) Precision(TM) Gamepad", 0, 10, 30, 40)
-    mapping.bind_axis_range("Logitech Logitech(R) Precision(TM) Gamepad", 1, 11, 30, 40)
+    name = "Logitech Logitech(R) Precision(TM) Gamepad"
+    mapping.bind_all_buttons(name, 9)
+    mapping.bind_axis_range(name, 0, 10, 30, 40)
+    mapping.bind_axis_range(name, 1, 11, 30, 40)
 
     # Select the API:
-    #	- JACK: Unix
-    #	- ALSA: Linux
-    #	- CoreMIDI: MacOSX
-    #	- WS_MM: Windows MM
-    gamepad2midi("JACK", mapping)
-
+    # - JACK: Unix
+    # - ALSA: Linux
+    # - CoreMIDI: MacOSX
+    # - WS_MM: Windows MM
+    gamepad2midi("ALSA", mapping)
 
 if __name__ == '__main__':
     main()
+
